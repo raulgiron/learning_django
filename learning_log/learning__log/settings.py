7a7daf66139065a7b083a87e6c12a9ed0e413e6f
Raul Giron (he/him/his) <raulgiron@hotmail.com>
2022-05-15 14:59:05 -0400

    GitGuardian Team -incident Django Secret Key corrected.-

--- learning_log/learning__log/settings.py
+++ learning_log/learning__log/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+# System Imports:
 from pathlib import Path
+
+# My Imports:
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sq8$u1z!#1aids#w6x#weeq#x5-_%fm$9h(#o=&i6!q&rqrlkn'
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
